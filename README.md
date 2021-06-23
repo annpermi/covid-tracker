b9488c11ef741ccbaf530a43a8dd7d9e40ce5ed4
annpermi <perminova.anna85@gmail.com>
2021-06-23 13:10:27 -0400

    Chart data

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
+# Covid19 online Tracker
+
+### Responsive React App
+
 > ReactJS,
+
+> React CountUp,
+
+> React Hook,
 
 > Axios,
 
 > Material-UI,
 
 > CSS Modules
+
+> CX Classnames
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api/';
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
 
 function Chart() {
+    const [dailyData, setDailyData ] = useState([]);
+    // state = {
+    //     dailyData: {}
+    // }
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData( await fetchDailyData()); /* the return promise because  its async*/
+        }
+
+        console.log(dailyData)
+
+        fetchAPI();
+    });
+
+    const lineChart = (
+        /* if we don't have the first day of the daily data available */
+        dailyData.length //0 
+        ? (
+        <Line 
+            data={{
+                /* map return the date */
+                labels: dailyData(({date}) => date),
+                datasets: [{
+                    data: dailyData(({confirmed}) => confirmed),
+                    label: 'Infected',
+                    borderColor: '#3333ff',
+                    fill: true,
+                }, {
+                    data: dailyData(({deaths}) => deaths),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(255, 0, 0, 0.5',
+                    fill: true,
+                }],
+            }}
+        />) : null
+    );
+
     return (
-        <div>
-            <h1>Chart</h1>
+        <div className={styles.container}>
+            {lineChart}
         </div>
     )
 }
